33df6c985bbef43d7f1f254e81280d1e1420a775
Toufiqul314 <49118001+Toufiqul314@users.noreply.github.com>
2022-08-25 19:58:55 +0600

    update

--- Loops.java
+++ Loops.java
@@ -1,10 +1,11 @@
 package java_lab;
 
 public class Loops {
+//(Display five messages) Write a program that displays Welcome to Java five times
 
     public static void main(String[] args) {
-        for (int i = 1; i <=5; i++) {
-            System.out.println(i+"."+"Welcome to Java");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + "." + "Welcome to Java");
         }
     }
 
